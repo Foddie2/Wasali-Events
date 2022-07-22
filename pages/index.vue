989f03cb2ebc1dcb3142f11816262a1d4b5904a8
Foddie2 <Bensonagala@live.com>
2022-07-22 16:42:21 +0300

    file setups

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
+<Navbar/>
 <Hero/>
 </template>
 
 <script>
+import Navbar from '~/components/Navbar.vue';
 import Hero from '~/components/Hero.vue';
 export default {
     name: "IndexPage",
-    components: { Hero }
+    components: { Navbar, Hero }
 }
 </script>
